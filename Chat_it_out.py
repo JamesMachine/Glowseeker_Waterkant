5237a5fdef1a9c6b16b891a300702cf6007f538b
JamesMachine <104810126+JamesMachine@users.noreply.github.com>
2024-05-29 08:50:01 +0200

    Update Chat_it_out.py

--- Chat_it_out.py
+++ Chat_it_out.py
@@ -8,7 +8,7 @@ wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     # st.audio(wav_audio_data, format='audio/wav')
-    audio_file= open(st.audio(wav_audio_data, format='audio/wav'), "rb")
+    audio_file= open(wav_audio_data, "rb")
     transcription = client.audio.transcriptions.create(
         model="whisper-1", 
         file=audio_file
