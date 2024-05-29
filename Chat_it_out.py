e61185475033956e68ac4ce6c996127c2172ec5f
JamesMachine <104810126+JamesMachine@users.noreply.github.com>
2024-05-29 08:42:34 +0200

    Update Chat_it_out.py

--- Chat_it_out.py
+++ Chat_it_out.py
@@ -1,7 +1,18 @@
 import streamlit as st
 from st_audiorec import st_audiorec
+from openai import OpenAI
+
+client = OpenAI(api_key=st.secrets.api_key)
 
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
+    # st.audio(wav_audio_data, format='audio/wav')
+    audio_file= open(st.audio(wav_audio_data, format='audio/wav'), "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1", 
+        file=audio_file
+    )
+    print(transcription.text)
+
+
