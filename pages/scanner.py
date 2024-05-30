c29ad75ea3a354b7759e717bb35e76238770c58f
JamesMachine <104810126+JamesMachine@users.noreply.github.com>
2024-05-30 17:17:26 +0200

    Update scanner.py

--- pages/scanner.py
+++ pages/scanner.py
@@ -1,23 +1,11 @@
 import streamlit as st
 
-#picture = st.camera_input("Scanner")
-#
-#if picture:
-#  st.image(picture)
-# st.write("Very Well")
+picture = st.camera_input("Scanner")
 
-from streamlit_webrtc import webrtc_streamer
-import av
+if picture:
+  st.image(picture)
+ st.write("Very Well")
 
-def video_frame_callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    flipped = img[::-1,:,:]
-
-    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
 
 
 
