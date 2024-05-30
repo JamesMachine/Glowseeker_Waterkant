bfa72d068ecea1a4325db18a15c6f11156ee9f40
JamesMachine <104810126+JamesMachine@users.noreply.github.com>
2024-05-30 15:24:26 +0200

    Update scanner.py

--- pages/scanner.py
+++ pages/scanner.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
-picture = st.camera_input("Scanner")
+#picture = st.camera_input("Scanner")
+#
+#if picture:
+#  st.image(picture)
+# st.write("Very Well")
 
-if picture:
-  st.image(picture)
-  st.write("Very Well")
+from streamlit_webrtc import webrtc_streamer
 
-
+webrtc_streamer(key="example")
 
 
 
